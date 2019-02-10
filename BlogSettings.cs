a31a26174667deeab9d189197223745b859121b6
Hayden Miedema <haydenmiedema@gmail.com>
2019-02-10 11:20:39 -0500

    initial commit with generated files

---
+++ BlogSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace meg_blog
+{
+    public class BlogSettings
+    {
+        public string Name { get; set; } = "meg_blog";
+        public string Description { get; set; } = "A place to meet new people and discover new food over amazing conversation.";
+        public string Owner { get; set; } = "Megan Miedema";
+        public int PostsPerPage { get; set; } = 2;
+        public int CommentsCloseAfterDays { get; set; } = 10;
+    }
+}
